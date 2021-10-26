b972991d3375b0e7fb2eaea7795548031c1cc656
Dmitrii Kuzmin <dmitrii_kuzmin@epam.com>
2021-10-26 21:53:03 +0400

    task 2

---
+++ Labs/LanguageTasks/SyntaxAnalyzer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace SyntaxAnalyzer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.InputEncoding = Encoding.Unicode;
+			Console.OutputEncoding = Encoding.Unicode;
+			Console.WriteLine("Enter code for analyser:");
+			var code = Console.ReadLine();
+			var analyser = new SyntaxAnalyzer();
+			try
+			{
+				var result = analyser.Run(code);
+				Console.WriteLine(result ? "Okay" : "It is not a while statement");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+	}
+}
---
+++ Labs/LanguageTasks/SyntaxAnalyzer/SyntaxAnalyzer.cs
@@ -0,0 +1,149 @@
+﻿using LexicalAnalyzer;
+using System;
+using System.Collections.Generic;
+
+namespace SyntaxAnalyzer
+{
+	public enum EntryType { Cmd, Var, Const, CmdPtr }
+	public enum Cmd { JMP, JZ, SET, ADD, }
+	public class SyntaxAnalyzer
+	{
+		private List<Lexeme> _lexemeList;
+		private IEnumerator<Lexeme> _lexemeEnumerator;
+		
+		public bool Run(string code)
+		{
+			var analyser = new Analyzer();
+			var result = analyser.Run(string.Join(Environment.NewLine, code));
+			if (!result)
+			{
+				throw new Exception("Errors were occurred in lexical analyze");
+			}
+			return IsWhileStatement(analyser.Lexemes);
+		}
+
+		private bool IsWhileStatement(List<Lexeme> lexemeList)
+		{
+			_lexemeList = lexemeList;
+			if (lexemeList.Count == 0) return false;
+			_lexemeEnumerator = lexemeList.GetEnumerator();
+
+			if (!_lexemeEnumerator.MoveNext() || _lexemeEnumerator.Current.Type != LexemeType.Do) { Error("Ожидается do", _lexemeList.IndexOf(_lexemeEnumerator.Current)); }
+			_lexemeEnumerator.MoveNext();
+
+			if (_lexemeEnumerator.Current == null || _lexemeEnumerator.Current.Type != LexemeType.While) { Error("Ожидается while", _lexemeList.IndexOf(_lexemeEnumerator.Current)); }
+
+			_lexemeEnumerator.MoveNext();
+			if (!IsCondition()) return false;
+
+			while (IsStatement());
+
+			if (_lexemeEnumerator.Current == null || _lexemeEnumerator.Current.Type != LexemeType.Loop) { Error("Ожидается loop", _lexemeList.IndexOf(_lexemeEnumerator.Current)); }
+
+			if (_lexemeEnumerator.MoveNext()) { Error("Лишние символы", _lexemeList.IndexOf(_lexemeEnumerator.Current)); }
+
+			return true;
+		}
+
+		private bool IsCondition()
+		{
+			if (!IsLogicalExpression()) return false;
+			while (_lexemeEnumerator.Current.Type == LexemeType.Or)
+			{
+				_lexemeEnumerator.MoveNext();
+				if (!IsLogicalExpression()) return false;
+			}
+			return true;
+		}
+
+		private bool IsLogicalExpression()
+		{
+			if (!RelationalExpression()) return false;
+			while (_lexemeEnumerator.Current.Type == LexemeType.And)
+			{
+				_lexemeEnumerator.MoveNext();
+				if (!RelationalExpression()) return false;
+			}
+			return true;
+		}
+
+		private bool RelationalExpression()
+		{
+			if (!IsOperand()) return false;
+			if (_lexemeEnumerator.Current.Type == LexemeType.Relation)
+			{
+				_lexemeEnumerator.MoveNext();
+				if (!IsOperand()) return false;
+			}
+			return true;
+		}
+
+		private bool IsOperand()
+		{
+			if (_lexemeEnumerator.Current == null || (_lexemeEnumerator.Current.Class != LexemeClass.Identifier && _lexemeEnumerator.Current.Class != LexemeClass.Constant))
+			{
+				Error("Ожидается переменная или константа", _lexemeList.IndexOf(_lexemeEnumerator.Current));
+				return false;
+			}
+			_lexemeEnumerator.MoveNext();
+			return true;
+		}
+
+		private bool IsLogicalOperation()
+		{
+			if (_lexemeEnumerator.Current == null || (_lexemeEnumerator.Current.Type != LexemeType.And && _lexemeEnumerator.Current.Type != LexemeType.Or))
+			{
+				Error("Ожидается логическая операция", _lexemeList.IndexOf(_lexemeEnumerator.Current));
+				return false;
+			}
+			_lexemeEnumerator.MoveNext();
+			return true;
+		}
+
+		private bool IsStatement()
+		{
+			if (_lexemeEnumerator.Current != null && _lexemeEnumerator.Current.Type == LexemeType.Loop) return false;
+
+			if (_lexemeEnumerator.Current == null || _lexemeEnumerator.Current.Class != LexemeClass.Identifier)
+			{
+				if (_lexemeEnumerator.Current.Type == LexemeType.Output)
+				{
+					_lexemeEnumerator.MoveNext();
+					if (!IsOperand()) return false;
+					return true;
+				}
+
+				Error("Ожидается переменная", _lexemeList.IndexOf(_lexemeEnumerator.Current));
+				return false;
+			}
+			_lexemeEnumerator.MoveNext();
+
+			if (_lexemeEnumerator.Current == null || _lexemeEnumerator.Current.Type != LexemeType.Assignment)
+			{
+				Error("Ожидается присваивание", _lexemeList.IndexOf(_lexemeEnumerator.Current));
+				return false;
+			}
+			_lexemeEnumerator.MoveNext();
+
+			if (!IsArithmeticExpression()) return false;
+
+			return true;
+		}
+
+		private bool IsArithmeticExpression()
+		{
+			if (!IsOperand()) return false;
+			while (_lexemeEnumerator.Current.Type == LexemeType.ArithmeticOperation)
+			{
+				_lexemeEnumerator.MoveNext();
+				if (!IsOperand()) return false;
+			}
+			return true;
+		}
+
+		private void Error(string message, int position)
+		{
+			throw new Exception($"{message} в позиции: {position}");
+		}
+	}
+}
